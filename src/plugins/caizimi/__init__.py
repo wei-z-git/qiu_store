d0c80c8acb09128e267665a4fffe995a9847f15e
wei-z <1419864987@qq.com>
2023-06-23 13:16:13 +0000

    Feature/caizimi

---
+++ src/plugins/caizimi/__init__.py
@@ -0,0 +1,15 @@
+from nonebot.plugin import PluginMetadata
+from . import (
+    caizimi,
+)
+
+# from .config import Config
+
+
+__plugin_meta__ = PluginMetadata(
+    name="猜字谜",
+    description="过审用的",
+    usage="没什么用",
+    # config=Config,
+    extra={},
+)
