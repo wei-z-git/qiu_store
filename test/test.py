05389823524987534f3a6660efcc967d3ee2b7cb
wei-z-git <Amadeus.LU.Rui@outlook.com>
2024-03-17 17:17:59 +0800

    1

--- test/test.py
+++ test/test.py
@@ -1,20 +1,24 @@
 import webuiapi
-
+import base64
+from PIL import Image
+import io
 # create API client
 api = webuiapi.WebUIApi()
 
 # create API client with custom host, port
-#api = webuiapi.WebUIApi(host='127.0.0.1', port=7860)
-
-# create API client with custom host, port and https
-#api = webuiapi.WebUIApi(host='webui.example.com', port=443, use_https=True)
+api = webuiapi.WebUIApi(host='127.0.0.1', port=7860)
 
 # create API client with default sampler, steps.
-#api = webuiapi.WebUIApi(sampler='Euler a', steps=20)
+#api = webuiapi.WebUIApi(sampler='Euler a', steps=20)\
 
-# optionally set username, password when --api-auth=username:password is set on webui.
-# username, password are not protected and can be derived easily if the communication channel is not encrypted.
+image_path = 'D:/AI/images/1.jpg'
+image = Image.open(image_path)
+
+
 # you can also pass username, password to the WebUIApi constructor.
-api.set_auth('username', 'password')
-result2 = api.img2img(images=[result1.image], prompt="cute cat", seed=5555, cfg_scale=6.5, denoising_strength=0.6)
+result2 = api.img2img(images=[image], prompt="cute cat", seed="-1", cfg_scale=6.5, denoising_strength=0.6)
 result2.image
+pil_image = Image.open(result2.image)
+
+# Display the image
+pil_image.show()
