694d4ebd97ad4cfe9b55eecbdbe0fc029e502dd3
wei-z-git <Amadeus.LU.Rui@outlook.com>
2024-03-20 21:47:19 +0800

    1

--- src/plugins/ImageStylerAI/styler.py
+++ src/plugins/ImageStylerAI/styler.py
@@ -11,9 +11,9 @@ from PIL import Image
 from io import BytesIO
 import httpx
 import json
-from nonebot.adapters.qqguild import Message
+from nonebot.adapters.qqguild import Message,MessageSegment
 from typing import Union
-
+import base64
 # from .config import plugin_config
 # from .SDUtils import AifadianUtils
 
@@ -21,20 +21,38 @@ get_image = on_command('t', priority=1)
 
 
 @get_image.got("img_attach", prompt="请发送图片")
-async def _(state: T_State, matcher: Matcher, img_attach: Message = Arg()):
-    img_attach: Message = Arg()
-    img_attach[0].data["url"]
-    data = json.loads(img_attach)
-    # img_url=img_attach.0.["data"]["url"]
+async def _(matcher: Matcher, img_attach: Message = Arg()):
+    img_url="https://"+img_attach[0].data["url"]
     with httpx.Client() as client:
         response = client.get(img_url)
-        if response.status_code == 200:
-            with open(save_path, 'wb') as f:
-                f.write(response.content)
-            print("图片下载成功！")
-        else:
+        if response.status_code != 200:
             print(f"下载失败，错误代码：{response.status_code}")
-    mask = Image.open(BytesIO(img))
+    # await matcher.send(MessageSegment.file_image(response.content))
+
+    url = 'http://127.0.0.1:7860/tagger/v1/interrogate'
+    model = 'wd-v1-4-moat-tagger.v2'
+    threshold = 0.35
+    image_base64 = base64.b64encode(response.content).decode('utf-8')
+
+    data = {
+        "image": image_base64,
+        "model": model,
+        "threshold": threshold
+    }
+
+    # 发送POST请求
+    response = httpx.post(url, json=data)
+
+    # 检查响应状态码
+    if response.status_code == 200: 
+        json_data = response.json()
+        # 处理返回的JSON数据
+        caption_dict = list(json_data['caption']['tag'].keys())
+        print(caption_dict)
+
+    else:
+        print('Error:', response.status_code)
+        print('Response body:', response.text)
     # if default.result:
     #     if not await SUPERUSER(bot, event):
     #         await set_mask_cmd.finish("仅超级用户可设置词云默认形状")
