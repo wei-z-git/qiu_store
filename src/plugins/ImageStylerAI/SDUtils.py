51a30e5e0cd960b624504f0d22f747951b495024
wei-z-git <Amadeus.LU.Rui@outlook.com>
2024-03-23 21:46:24 +0800

    1

--- src/plugins/ImageStylerAI/SDUtils.py
+++ src/plugins/ImageStylerAI/SDUtils.py
@@ -1,56 +1,41 @@
 from typing import Dict
 from PIL import Image
 import httpx
-import hashlib
-import time
-import json
-import base64
 from io import BytesIO
+import webuiapi
+import base64
+from PIL import Image
 
 
 class SDUtils:
     def __init__(self):
-        self.host = "http://127.0.0.1:7860"
+        self.host = "127.0.0.1"
+        self.port = "7860"
+        self.model = 'wd-v1-4-moat-tagger.v2'
+        self.threshold = 0.35
+        self.protocol = "http"
 
-
-    async def img2tags(self, img) -> list:
-        api="/tagger/v1/interrogate"
-        url=self.host+api
-        image_path = 'D:/AI/images/1.jpg'
-        model = 'wd-v1-4-moat-tagger.v2'
-        threshold = 0.35
-        image = Image.open(image_path)
-        with open(image_path, 'rb') as file:
-            image_data = file.read()
-            base64_image = base64.b64encode(image_data).decode('utf-8')
+    async def img2tags(self, img_base64) -> str:
+        api = "/tagger/v1/interrogate"
+        url = f"{self.protocol}://{self.host}:{self.port}{api}"
         data = {
-            "image": base64_image,
-            "model": model,
-            "threshold": threshold
+            "image": img_base64,
+            "model": self.model,
+            "threshold": self.threshold
         }
-        response = httpx.post(url, json=data)
-        if response.status_code == 200: 
-            json_data = response.json()
-            caption_dict = list(json_data['caption']['tag'].keys())
-            print(caption_dict)
+        # Get tags and serialize
+        json_data = httpx.post(url, json=data).json()
+        # 取出tags, 并拼接为str
+        caption_dict = json_data['caption']['tag'].keys()
+        caption_str = ', '.join(caption_dict)
+        return caption_str
 
-        else:
-            print('Error:', response.status_code)
-            print('Response body:', response.text)
-        
-    @staticmethod
-    async def generate_QRcode(plan_id) -> bytes:
-        '''
-        根据plan_id返回QR code
-        '''
-        plan_text = "https://afdian.net/item/"+plan_id
-        qr = qrcode.QRCode(version=1, box_size=10, border=4)
-        qr.add_data(plan_text)
-        qr.make(fit=True)
-        # 创建PIL图片对象
-        img = qr.make_image(fill_color="black", back_color="white")
-        # 将PIL图片对象转换为bytes对象
+    async def img2img(self, tags, img_pil) -> bytes:
+        api = webuiapi.WebUIApi(host=self.host, port=self.port)
+        result = api.img2img(
+            images=[img_pil], prompt=tags, seed="-1", cfg_scale=6.5, denoising_strength=0.6)
+        result_img = result.image
         img_byte_array = BytesIO()
-        img.save(img_byte_array, format='PNG')
-        qr_code_bytes = img_byte_array.getvalue()
-        return qr_code_bytes
+        result_img.save(img_byte_array, format='PNG')
+        result_bytes = img_byte_array.getvalue()
+        return result_bytes
--- src/plugins/ImageStylerAI/styler.py
+++ src/plugins/ImageStylerAI/styler.py
@@ -1,65 +1,32 @@
 from nonebot import on_command
-# from nonebot.adapters import Bot, Message
 from nonebot.matcher import Matcher
-from nonebot.adapters.qqguild.event import AtMessageCreateEvent, MessageCreateEvent, DirectMessageCreateEvent
-from nonebot.adapters.qqguild.exception import AuditException
-from nonebot.typing import T_State
 from nonebot.params import Depends, Arg, ArgPlainText
 from nonebot.rule import to_me
-from nonebot.log import logger
-from PIL import Image
-from io import BytesIO
+from nonebot.adapters.qqguild.event import AtMessageCreateEvent, MessageCreateEvent, DirectMessageCreateEvent
+
 import httpx
-import json
 from nonebot.adapters.qqguild import Message,MessageSegment
-from typing import Union
 import base64
 # from .config import plugin_config
-# from .SDUtils import AifadianUtils
-
+from .SDUtils import SDUtils
+from PIL import Image
+from io import BytesIO
+from typing import Union
 get_image = on_command('t', priority=1)
 
 
-@get_image.got("img_attach", prompt="请发送图片")
+@get_image.got("img_attach", prompt="请发送图片",)
 async def _(matcher: Matcher, img_attach: Message = Arg()):
-    img_url="https://"+img_attach[0].data["url"]
+    img_url = "https://"+img_attach[0].data["url"]
     with httpx.Client() as client:
         response = client.get(img_url)
         if response.status_code != 200:
             print(f"下载失败，错误代码：{response.status_code}")
-    # await matcher.send(MessageSegment.file_image(response.content))
+    pil_image = Image.open(BytesIO(response.content))
 
-    url = 'http://127.0.0.1:7860/tagger/v1/interrogate'
-    model = 'wd-v1-4-moat-tagger.v2'
-    threshold = 0.35
     image_base64 = base64.b64encode(response.content).decode('utf-8')
 
-    data = {
-        "image": image_base64,
-        "model": model,
-        "threshold": threshold
-    }
-
-    # 发送POST请求
-    response = httpx.post(url, json=data)
-
-    # 检查响应状态码
-    if response.status_code == 200: 
-        json_data = response.json()
-        # 处理返回的JSON数据
-        caption_dict = list(json_data['caption']['tag'].keys())
-        print(caption_dict)
-
-    else:
-        print('Error:', response.status_code)
-        print('Response body:', response.text)
-    # if default.result:
-    #     if not await SUPERUSER(bot, event):
-    #         await set_mask_cmd.finish("仅超级用户可设置词云默认形状")
-    #     mask.save(plugin_config.get_mask_path(), format="PNG")
-    #     await set_mask_cmd.finish("词云默认形状设置成功")
-    # else:
-    #     mask.save(plugin_config.get_mask_path(mask_key), format="PNG")
-    #     await set_mask_cmd.finish("词云形状设置成功")
-
-# <class 'nonebot.adapters.qqguild.message.Message'>
+    sd = SDUtils()
+    tags = await sd.img2tags(image_base64)
+    img_new = await sd.img2img(tags, pil_image)
+    await matcher.send(MessageSegment.file_image(img_new))
